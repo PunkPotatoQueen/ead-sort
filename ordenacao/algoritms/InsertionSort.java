035a66f4e6be860587849309e054b2e93aad0128
Izadora Freitas <109389495+PunkPotatoQueen@users.noreply.github.com>
2023-03-07 20:46:30 +0000

    tentiva 1 de muitas

--- ordenacao/algoritms/InsertionSort.java
+++ ordenacao/algoritms/InsertionSort.java
@@ -5,10 +5,23 @@ import ordenacao.Sorter;
 public class InsertionSort implements Sorter{
 
     @Override
-    public int[] sort(int[] elements) {
-        int [] sorted = elements.clone();
-        //TODO Implementar
-        return sorted;
+    public int[] sort(int[] arr) {
+        //int [] sorted = arr.clone();
+        int chave = 0;
+        int contArr = 0;
+        int a =0;
+        for (int i = 1 ; i < arr.length; i++){
+            chave = arr[i];
+            contArr = i -1;
+            while (contArr>0 && arr[i-1]>chave){
+                a = arr[contArr];
+                arr [contArr] = arr [contArr-1];
+                arr [contArr-1] = a;
+            }
+            contArr--;
+        }
+        
+        return arr;
     }
 
     @Override
